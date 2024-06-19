c36f7f0d031eb73722f419bc6d85ba7191a97498
Lee Goddard <leegee@gmail.com>
2024-06-19 10:43:48 +0200

    More familliar time display

--- src/components/TimeDisplay.svelte
+++ src/components/TimeDisplay.svelte
@@ -1,20 +1,34 @@
 <script>
     import { currentTime } from "../stores/current-time";
+
+    // Fastest, not prettiest
+    function formatTime(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const secs = Math.floor(seconds % 60);
+        const milliseconds = Math.floor((seconds % 1) * 1000);
+
+        const hoursString = (hours < 10 ? "0" : "") + hours;
+        const minutesString = (minutes < 10 ? "0" : "") + minutes;
+        const secondsString = (secs < 10 ? "0" : "") + secs;
+        const millisecondsString =
+            (milliseconds < 100 ? (milliseconds < 10 ? "00" : "0") : "") +
+            milliseconds;
+
+        return `${hoursString}::${minutesString}::${secondsString},${millisecondsString}`;
+    }
 </script>
 
 <span id="time-container">
-    Time: <span id="time-value">
-        {$currentTime.toFixed(2)}
-    </span>
+    {formatTime($currentTime)}
 </span>
 
 <style>
     #time-container {
         margin-left: 1em;
-    }
-
-    #time-value {
         font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
             "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+        font-size: x-large;
+        opacity: 50%;
     }
 </style>
