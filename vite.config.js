1e9b67655825273ea5828ad2b21564a55c5a014c
Lee Goddard <leegee@gmail.com>
2024-07-03 17:37:17 +0200

    relative paths

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( {
+  base: './',
   plugins: [svelte()],
 })
