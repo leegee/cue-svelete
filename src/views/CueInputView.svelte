4fe40bfc61149fb6eeefc35282fee61fb00a509c
Lee Goddard <leegee@gmail.com>
2024-10-15 22:24:22 +0200

    Allow overlays

--- src/views/CueInputView.svelte
+++ src/views/CueInputView.svelte
@@ -1,68 +1,66 @@
 <script>
-    import VideoPlayer from "../components/Video.svelte";
-    import CueGrid from "../components/CueGrid.svelte";
-    import LoadSRT from "../components/LoadSRT.svelte";
-    import SaveMIDI from "../components/SaveMIDI.svelte";
-    import SaveSRT from "../components/SaveSRT.svelte";
-    import TimeDisplay from "../components/TimeDisplay.svelte";
-    import AddCue from "../components/AddCue.svelte";
+  import VideoPlayer from "../components/Video.svelte";
+  import CueGrid from "../components/CueGrid.svelte";
+  import LoadSRT from "../components/LoadSRT.svelte";
+  import SaveMIDI from "../components/SaveMIDI.svelte";
+  import SaveSRT from "../components/SaveSRT.svelte";
+  import SaveMusicXML from "../components/SaveMusicXML.svelte";
+  import TimeDisplay from "../components/TimeDisplay.svelte";
+  import AddCue from "../components/AddCue.svelte";
 
-    export let videoUrl;
+  export let videoUrl;
 </script>
 
 <section>
-    <div id="video-and-controls">
-        <VideoPlayer {videoUrl} />
+  <div id="video-and-controls">
+    <VideoPlayer {videoUrl} />
 
-        <div id="controls-container">
-            <AddCue />
-            <TimeDisplay />
-        </div>
+    <div id="controls-container">
+      <TimeDisplay />
+      <AddCue />
     </div>
+  </div>
 
-    <div id="cue-grid">
-        <CueGrid />
-    </div>
+  <div id="cue-grid">
+    <CueGrid />
+  </div>
 
-    <footer>
-        <LoadSRT />
-        <SaveSRT />
-        <SaveMIDI />
-    </footer>
+  <footer>
+    <LoadSRT />
+    <SaveSRT />
+    <SaveMIDI />
+    <SaveMusicXML />
+  </footer>
 </section>
 
 <style>
-    section {
-        height: 100vh;
-    }
+  section {
+    height: 100vh;
+  }
 
-    #cue-grid {
-        height: calc(75vh - 32pt);
-    }
+  #cue-grid {
+    height: calc(75vh - 32pt);
+  }
 
-    #video-and-controls {
-        height: 25vh;
-        display: flex;
-        align-items: center;
-        justify-content: space-evenly;
-    }
+  #video-and-controls {
+    height: 25vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+  }
 
-    #controls-container {
-        display: flex;
-        align-items: stretch;
-        flex-direction: column;
-    }
+  #controls-container {
+    display: flex;
+    align-items: stretch;
+    flex-direction: column;
+  }
 
-    footer {
-        position: absolute;
-        bottom: 0;
-        height: 32pt;
-        width: auto;
-        left: 50%;
-        transform: translate(-50%, 0);
-        display: flex;
-        align-items: center;
-        justify-content: space-evenly;
-        font-size: small;
-    }
+  footer {
+    height: 32pt;
+    width: auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    font-size: small;
+  }
 </style>
